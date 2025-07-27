ccc059e32659ddb3a9a0e0e78683331abed80e12
kasra <kasrarahmanian3@gmail.com>
2025-07-27 14:01:10 +0330

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,14 +10,6 @@ export default defineNuxtConfig({
   },
 
   css: ["~/assets/css/main.css"],
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-
   modules: ["@nuxt/icon", "@nuxt/image", "@nuxt/ui", "@pinia/nuxt"],
 
   vite: {
