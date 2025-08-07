8760599adb5ae00f1a63d2e22b175121e303c205
kasra <kasrarahmanian3@gmail.com>
2025-08-07 13:35:56 +0330

    fixed auth and set tokens in cookie

--- components/auth/VerifyOtp.vue
+++ components/auth/VerifyOtp.vue
@@ -1,15 +1,17 @@
 <template>
   <BaseModal @close="closeModal">
     <div class="relative flex flex-col items-center gap-5 otp-wrapper">
-      <img src="/logo/logo.png" alt="Logo" class="w-[180px] h-[160px] mx-auto" />
+      <img
+        src="/logo/logo.png"
+        alt="Logo"
+        class="w-[180px] h-[160px] mx-auto" />
 
       <p
-        class="font-['iran-yekan-Light'] w-1/2 text-center mx-auto py-[15px] text-base mb-5"
-      >
+        class="font-['iran-yekan-Light'] w-1/2 text-center mx-auto py-[15px] text-base mb-5">
         کد 4 رقمی ارسال شده به شماره
         <span
           class="font-['iran-yekan-num-Regular'] text-blue-dark border-b border-blue-dark"
-          >{{phoneNumber}}</span
+          >{{ phoneNumber }}</span
         >
         را وارد نمایید
       </p>
@@ -27,35 +29,33 @@
           @keydown="onKeydown(index, $event)"
           :disabled="expired || loading"
           required
-          class="w-[45px] h-[50px] text-xl text-center border-2 border-gray-400 rounded-lg outline-none transition-colors duration-200 ease-in-out dir-ltr text-blue-dark font-['iran-yekan-num-DemiBold'] focus:border-blue-dark disabled:bg-gray-200 disabled:cursor-not-allowed"
-        />
+          class="w-[45px] h-[50px] text-xl text-center border-2 border-gray-400 rounded-lg outline-none transition-colors duration-200 ease-in-out dir-ltr text-blue-dark font-['iran-yekan-num-DemiBold'] focus:border-blue-dark disabled:bg-gray-200 disabled:cursor-not-allowed" />
       </div>
 
       <div
         v-if="!expired"
-        class="absolute right-0 bottom-[130px] font-['iran-yekan-num-Regular'] text-xs text-gray-800"
-      >
+        class="absolute right-0 bottom-[130px] font-['iran-yekan-num-Regular'] text-xs text-gray-800">
         🕒 زمان باقی‌مانده: {{ formattedTime }}
       </div>
       <div v-else class="absolute right-0 bottom-[130px] text-red-500 text-sm">
         ⛔ کد منقضی شد
       </div>
 
-      <div v-if="errorMessage" class="text-red-500 text-sm mt-2">{{ errorMessage }}</div>
+      <div v-if="errorMessage" class="text-red-500 text-sm mt-2">
+        {{ errorMessage }}
+      </div>
 
       <button
         class="py-2.5 px-5 text-base bg-blue-500 text-white rounded-lg cursor-pointer transition-colors duration-300 disabled:bg-gray-400 disabled:cursor-not-allowed"
         @click="verifyOtp"
-        :disabled="!isComplete || expired || loading"
-      >
+        :disabled="!isComplete || expired || loading">
         {{ loading ? "در حال ارسال..." : "ارسال کد" }}
       </button>
 
       <button
         class="absolute left-0 bottom-[130px] bg-transparent border-none text-blue-600 hover:underline disabled:text-gray-400 disabled:no-underline disabled:cursor-not-allowed"
         @click="resetTimer"
-        :disabled="!expired || loading"
-      >
+        :disabled="!expired || loading">
         ارسال دوباره کد
       </button>
     </div>
@@ -115,6 +115,7 @@ const resetTimer = () => {
   errorMessage.value = "";
   startTimer();
   nextTick(() => otpRefs.value[0]?.focus());
+  verifyOtp();
 };
 
 const onInput = async (index: number) => {
@@ -148,16 +149,19 @@ const verifyOtp = async () => {
   try {
     // درخواست به سرور ارسال کن (آدرس و payload رو با بک‌اند چک کن)
     const response = await $axios.post("/auth/verify-otp", {
-       phoneNumber: phoneNumber.value,
-       otp: otpCode.value,
+      phoneNumber: phoneNumber.value,
+      otp: otpCode.value,
     });
 
-    if (response.status === 200) {
+    if (response.status === 201) {
       const authStore = useAuthStore();
+
+      console.log("accessToken from response:", response.data.accessToken);
       authStore.setTokens(
-      response.data.accessToken,
-      response.data.refreshToken
-);
+        response.data.accessToken,
+        response.data.refreshToken
+      );
+      console.log("after setTokens - accessToken:", authStore.getAccessToken());
       emit("onVerified");
     } else {
       errorMessage.value = "کد وارد شده اشتباه است.";
