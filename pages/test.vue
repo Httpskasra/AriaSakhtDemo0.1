0c14cdb816b257e146f08cf8f06ed17acbf83f09
root <root@ariasakht.openstacklocal>
2025-08-09 16:34:17 +0330

    change test page for auth/me

--- pages/test.vue
+++ pages/test.vue
@@ -1,12 +1,17 @@
 <template>
   <h1>صفحه تست</h1>
 </template>
+
 <script setup lang="ts">
+import { onMounted } from "vue";
 const { $axios } = useNuxtApp();
-try {
-  const { data } = await $axios.get("/auth/me");
-  console.log(data.data);
-} catch (error) {
-  console.log(error);
-}
+
+onMounted(async () => {
+  try {
+    const { data } = await $axios.get("/auth/me");
+    console.log(data.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 </script>
